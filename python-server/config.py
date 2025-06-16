7aa3de01ff4c3a9d9909e2499f20a5e3e8a4fe72
ammaara <106888899+ammaara@users.noreply.github.com>
2025-06-16 13:53:23 -0400

    clean up unused files

--- python-server/config.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseSettings
-
-class Settings(BaseSettings):
-    database_url: str = "postgresql+asyncpg://user:pass@localhost/knaps"
-    node_env: str = "development"
-
-settings = Settings()
